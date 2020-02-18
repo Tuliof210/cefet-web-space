e670d907806f1eaad3aece653ed4873c3cb6df9b
Tulio Ferreira <tuliof210@gmail.com>
2020-02-18 01:49:42 -0300

    Exercicio alien finalizado

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,12 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+var itens = document.querySelectorAll('.botao-expandir-retrair')
+
+for (let i of itens) {
+    i.addEventListener('click', function(e) {
+        let pEl = e.currentTarget;
+        let parent = pEl.parentElement; //parent of button => p
+        parent.classList.toggle('expandido');
+    })
+}
--- exercicio2.js
+++ exercicio2.js
@@ -2,10 +2,40 @@
 // Este arquivo ainda não está incluído no arquivo HTML
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+    todasAsImagens = [
+        'philae-parts.jpg',
+        'philae-rosetta.jpg',
+        'philae-separation.jpg',
+        'philae-67-picture.jpg',
+        'philae-collecting.jpg'
+    ];
+
+console.log(`${servidorDasImagens}/${todasAsImagens[2]}`); //template string
+
+var imgs = document.querySelector('#slide')
+var ant = document.querySelector('#anterior')
+var prox = document.querySelector('#proximo')
+
+var atual = 0;
+
+ant.addEventListener('click', function(e) {
+
+    if (atual == 0) {
+        atual = todasAsImagens.length;
+    }
+    console.log(atual - 1)
+    imgs.src = `${servidorDasImagens}/${todasAsImagens[atual-1]}`
+
+    atual--;
+})
+
+prox.addEventListener('click', function(e) {
+
+    if (atual == (todasAsImagens.length - 1)) {
+        atual = -1
+    }
+    console.log(atual + 1)
+    imgs.src = `${servidorDasImagens}/${todasAsImagens[atual+1]}`
+
+    atual++;
+})
